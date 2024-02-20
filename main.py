47618b6c474952faaeca87d8409ea41e5c89f794
thegreenlion42 <124219051+thegreenlion42@users.noreply.github.com>
2024-02-20 20:55:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ ota_updater.download_and_install_update_if_available()
 
 #Es lebt!
 
-
-
-
 led_pin = 8  # Default on-board RGB LED GPIO08 does not work
 
 led = Pin(led_pin, Pin.OUT)
 
 for i in range(10):
-  led.off()
-  time.sleep_ms(500)
-  led.on()
+  led.value(not led.value())
   time.sleep_ms(500)
   print("Blink ", i+1)
