5df48ca01353cf6c1464903d277b6c097b13d6eb
thegreenlion42 <124219051+thegreenlion42@users.noreply.github.com>
2024-02-20 19:35:28 +0100

    #Es lebt!
    First test change

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
+#Es lebt!
+firmware_url = "https://github.com/thegreenlion42/BathVentRegulator/new/main/"
 
-firmware_url = "https://raw.githubusercontent.com/pierreyvesbaloche/kevinmca_ota/main/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test_ota.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
