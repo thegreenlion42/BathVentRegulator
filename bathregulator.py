0ac63d74cb0408234a971fdc8e607fba3e13b3cc
thegreenlion42 <124219051+thegreenlion42@users.noreply.github.com>
2024-02-20 21:24:59 +0100

    Update bathregulator.py

--- bathregulator.py
+++ bathregulator.py
@@ -1,13 +1,12 @@
 from machine import Pin
 import time
 
-#Es lebt!
+def Do():
+    led_pin = 8  # Default on-board RGB LED GPIO08 does not work
 
-led_pin = 8  # Default on-board RGB LED GPIO08 does not work
+    led = Pin(led_pin, Pin.OUT)
 
-led = Pin(led_pin, Pin.OUT)
-
-for i in range(10):
-  led.value(not led.value())
-  time.sleep_ms(500)
-  print("Blink ", i+1)
+    for i in range(10):
+      led.value(not led.value())
+      time.sleep_ms(500)
+      print("Blink ", i+1)
