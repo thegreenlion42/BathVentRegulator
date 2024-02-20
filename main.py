3bbba9672b7780aab3f45be2a2597ab09b3d5294
thegreenlion42 <124219051+thegreenlion42@users.noreply.github.com>
2024-02-20 20:49:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ led_pin = 8  # Default on-board RGB LED GPIO08 does not work
 led = Pin(led_pin, Pin.OUT)
 
 for i in range(10):
-  led.on()
-  time.sleep_ms(500)
   led.off()
+  time.sleep_ms(500)
+  led.on()
   time.sleep_ms(500)
   print("Blink ", i+1)
