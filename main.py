9f497bc969d6a9e27be2e5ea13fdadebf3a3b7ca
thegreenlion42 <124219051+thegreenlion42@users.noreply.github.com>
2024-02-20 19:41:19 +0100

    added blink sketch

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
-#Es lebt!
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+led_pin = 3  # Default on-board RGB LED GPIO08 does not work
+
+led = Pin(led_pin, Pin.OUT)
+
 firmware_url = "https://github.com/thegreenlion42/BathVentRegulator/new/main/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
+
+#Es lebt!
+
+led_pin = 3  # Default on-board RGB LED GPIO08 does not work
+
+led = Pin(led_pin, Pin.OUT)
+
+for i in range(10):
+  led.on()
+  time.sleep_ms(500)
+  led.off()
+  time.sleep_ms(500)
+  print("Blink ", i+1)
